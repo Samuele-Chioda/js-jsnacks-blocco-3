20b7a27f4a5d28e1cb900dd0be6696a0f579ed86
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-14 17:50:21 +0200

    TuttiFrutti

---
+++ js/array-lista.js
@@ -0,0 +1,18 @@
+// array frigorifero con lista dei frutti all'interno
+const frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+// aggiungo elemento pesca alla lista dei frutti
+frigorifero.push('pesca');
+
+let cocomeroPresente = false;
+// ciclo for per controllare elementi frigorifero
+for (let frutto of frigorifero) {
+    if (frutto === 'cocomero') {
+        cocomeroPresente = true;
+    }
+}
+// condizioni di stampa in console
+if (cocomeroPresente) {
+    console.log("Trovato! Devo solo preparare il cocktail.");
+} else {
+    console.log("Oh no, devo uscire a comprare il cocomero!");
+}
