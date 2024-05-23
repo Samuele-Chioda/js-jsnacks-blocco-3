e7fedf21be890a9336456abc1c89ea08ac468315
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-23 12:20:06 +0200

    array auto

---
+++ js/snackobjects1.js
@@ -0,0 +1,86 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+
+let auto = [{
+    marca:('ford'),
+    modello:('kuga'),
+    alimentazione:('benzina')
+},
+{
+    marca:('ford'),
+    modello:('kuga'),
+    alimentazione:('benzina')
+},
+{
+    marca:('ford'),
+    modello:('kuga'),
+    alimentazione:('benzina')
+},
+{
+    marca:('ford'),
+    modello:('kuga'),
+    alimentazione:('benzina')
+},
+{
+    marca:('ford'),
+    modello:('kuga'),
+    alimentazione:('benzina')
+},
+{
+    marca:('audi'),
+    modello:('Auno'),
+    alimentazione:('diesel')
+},
+{
+    marca:('audi'),
+    modello:('Auno'),
+    alimentazione:('diesel')
+},
+{
+    marca:('audi'),
+    modello:('Auno'),
+    alimentazione:('diesel')
+},
+{
+    marca:('audi'),
+    modello:('Auno'),
+    alimentazione:('diesel')
+},
+{
+    marca:('audi'),
+    modello:('Auno'),
+    alimentazione:('diesel')
+},
+{
+    marca:('fiat'),
+    modello:('fiorino'),
+    alimentazione:('gpl')
+},
+{
+    marca:('fiat'),
+    modello:('fiorino'),
+    alimentazione:('gpl')
+},
+{
+    marca:('fiat'),
+    modello:('fiorino'),
+    alimentazione:('gpl')
+},
+{
+    marca:('fiat'),
+    modello:('fiorino'),
+    alimentazione:('gpl')
+},
+{
+    marca:('fiat'),
+    modello:('fiorino'),
+    alimentazione:('gpl')
+},
+];
+
+auto.forEach(element => {
+    console.log(marca);
+});
